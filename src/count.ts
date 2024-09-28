677f3af09e1f8e0b93270bd548fd671f7d5c2384
相甫 <xiangfu_git@163.com>
2024-09-28 22:52:32 +0800

    feat: 添加异步调试

--- src/count.ts
+++ src/count.ts
@@ -1,3 +1,8 @@
 export const add = (a: number, b: number): number => {
   return a + b
 }
+
+export const addAsync = async (a: number, b: number): Promise<number> => {
+  const res = a + b
+  return res
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import { add } from './count'
+import { add, addAsync } from './count'
 import type { Method } from './type'
 // 入口文件
 const test: Method = 'get'
 console.log('🚀🚀🚀======>>>test', test)
 
 const res = add(1, 2)
-console.log('🚀🚀🚀======>>>res', res)
+console.log('🚀🚀🚀======>>>add', res)
+
+const testAsync = async () => {
+  const res = await addAsync(1, 2)
+  console.log('🚀🚀🚀======>>>testAsync', res)
+}
+
+testAsync()
